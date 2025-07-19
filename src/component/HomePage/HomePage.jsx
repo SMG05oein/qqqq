b12621390f900abfe718c1d19d3e3f2585a16e19
SMG <SMG05eoin@gmail.com>
2025-07-19 10:59:51 +0900

    CONTENTS CSS변경 및 리팩토링

--- src/component/HomePage/HomePage.jsx
+++ src/component/HomePage/HomePage.jsx
@@ -5,7 +5,7 @@ import "./HomePage.style.css"
 
 const HomePage = () => {
     return (
-        <div className="HomePage CONTENTS justify-content-center">
+        <div className="HomePage justify-content-center">
             <Container>
                 {/*관리*/}
                 <Row className={"NotFlex"}>
@@ -47,17 +47,17 @@ const HomePage = () => {
                 </Row>
 
                 {/*/!*서비스 버로가기*!/*/}
-                {/*<Row className={"NotFlex"}>*/}
-                {/*    <div className={"HomeBox"}>*/}
-                {/*        <Row>*/}
-                {/*            <Col>잔액: 1000원</Col>*/}
-                {/*        </Row>*/}
-                {/*        <Row>*/}
-                {/*            <Col><Link to={"/"}>이용내역</Link></Col>*/}
-                {/*            <Col><Link to={"/"}>충전</Link></Col>*/}
-                {/*        </Row>*/}
-                {/*    </div>*/}
-                {/*</Row>*/}
+                <Row className={"NotFlex"}>
+                    <div className={"HomeBox"}>
+                        <Row>
+                            <Col>잔액: 1000원</Col>
+                        </Row>
+                        <Row>
+                            <Col><Link to={"/"}>이용내역</Link></Col>
+                            <Col><Link to={"/"}>충전</Link></Col>
+                        </Row>
+                    </div>
+                </Row>
             </Container>
         </div>
     );
