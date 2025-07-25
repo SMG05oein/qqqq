a772123916236e4af13bc1aac722be78f3b50413
SMG <SMG05eoin@gmail.com>
2025-07-26 00:18:44 +0900

    GPtversion 웹앱 형식 세팅

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import {BrowserRouter} from "react-router-dom";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import LoginProvider from "./State/LoginState";
 import PayingBarOpenState from "./State/PayingBarOpenState";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+serviceWorkerRegistration.register();
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const queryClient = new QueryClient();
